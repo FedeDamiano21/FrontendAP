4b9f231a942566d9c07f16e3e96a6659a1d21ec5
FedeDamiano21 <federicodamiano@hotmail.com>
2023-01-03 16:39:07 -0300

    commit

---
+++ proyecto-final-AP/BackEnd/src/test/java/com/portfolio/FD/FdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.FD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
