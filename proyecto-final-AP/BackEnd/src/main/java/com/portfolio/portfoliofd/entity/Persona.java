c8694d0766139972fbc137ae7a584043d99b4dca
FedeDamiano21 <federicodamiano@hotmail.com>
2023-01-20 17:11:35 -0300

    ....

--- proyecto-final-AP/BackEnd/src/main/java/com/portfolio/portfoliofd/entity/Persona.java
+++ proyecto-final-AP/BackEnd/src/main/java/com/portfolio/portfoliofd/entity/Persona.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 
 
 
 
-
+@Getter @Setter
 @Entity
 public class Persona {
     
@@ -21,4 +23,14 @@ public class Persona {
     
     private String nombre;
     private String apellido;
+    
+    public Persona() {
+    }
+    
+    public Persona(Long id, String nombre, String apellido){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
 }
