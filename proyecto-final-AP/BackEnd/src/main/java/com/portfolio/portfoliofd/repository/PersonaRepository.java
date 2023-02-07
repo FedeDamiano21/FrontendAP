08ce82b116bc7aa8f0c43586db587acc76973b4f
FedeDamiano21 <federicodamiano@hotmail.com>
2023-02-07 15:29:31 -0300

    commit

---
+++ proyecto-final-AP/BackEnd/src/main/java/com/portfolio/portfoliofd/repository/PersonaRepository.java
@@ -0,0 +1,11 @@
+package com.portfolio.portfoliofd.repository;
+
+import com.portfolio.portfoliofd.entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PersonaRepository extends JpaRepository <Persona, Long>{
+    
+}
